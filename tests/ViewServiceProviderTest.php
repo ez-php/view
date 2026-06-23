fbaa208c2517fee41e2cb469947c53efdab0bec8
home <andreas.uretschnig@gmail.com>
2026-06-24 00:57:03 +0200

    Upgrade ez-php modules to version 1.12.0
    This commit updates all ez-php modules to version 1.12.0. It also includes adjustments to composer files and introduces support for the Typesense search driver in the search module.

---
+++ tests/ViewServiceProviderTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use EzPhp\Application\Application;
+use EzPhp\View\View;
+use EzPhp\View\ViewEngine;
+use EzPhp\View\ViewServiceProvider;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+/**
+ * Smoke test: ViewServiceProvider registers and boots its bindings in a minimal
+ * application context without error.
+ *
+ * @package Tests
+ */
+#[CoversClass(ViewServiceProvider::class)]
+#[UsesClass(View::class)]
+#[UsesClass(ViewEngine::class)]
+final class ViewServiceProviderTest extends TestCase
+{
+    /**
+     * @param Application $app
+     *
+     * @return void
+     */
+    protected function configureApplication(Application $app): void
+    {
+        $app->register(ViewServiceProvider::class);
+    }
+
+    /**
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        View::resetEngine();
+        parent::tearDown();
+    }
+
+    /**
+     * @return void
+     * @throws \ReflectionException
+     */
+    public function test_view_engine_is_bound(): void
+    {
+        $this->assertInstanceOf(ViewEngine::class, $this->app()->make(ViewEngine::class));
+    }
+
+    /**
+     * @return void
+     * @throws \ReflectionException
+     */
+    public function test_view_engine_binding_is_a_singleton(): void
+    {
+        $this->assertSame(
+            $this->app()->make(ViewEngine::class),
+            $this->app()->make(ViewEngine::class),
+        );
+    }
+}
